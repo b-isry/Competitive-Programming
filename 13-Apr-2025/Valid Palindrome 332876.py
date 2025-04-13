7b8074a82a33b80b607be58e3d2d617507e6236b
Bisrat Teshome <135213040+b-isry@users.noreply.github.com>
2025-04-13 17:54:07 +0300

    Created 'Valid Palindrome' by A2SV Hub

---
+++ 13-Apr-2025/Valid Palindrome 332876.py
@@ -0,0 +1,13 @@
+# Problem: Valid Palindrome - https://leetcode.com/problems/valid-palindrome/description/
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+        l, r = 0, len(s)-1
+
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
