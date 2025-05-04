b5011501e8564a77ac026bdc40113d1239ee6790
Bisrat Teshome <135213040+b-isry@users.noreply.github.com>
2025-05-04 23:00:24 +0300

    Created 'Josephus Problem' by A2SV Hub

---
+++ 04-May-2025/Josephus Problem 348263.py
@@ -0,0 +1,10 @@
+# Problem: Josephus Problem - https://www.geeksforgeeks.org/josephus-problem/
+
+def Josh(person, k, index):
+  if len(person) == 1:
+    print(person[0])
+    return
+
+  index = ((index+k)%len(person))
+  person.pop(index)
+  Josh(person,k,index)
