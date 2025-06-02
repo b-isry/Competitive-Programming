e3437ba87852d9e50efbba189355a5f8ebcacce7
Bisrat Teshome <135213040+b-isry@users.noreply.github.com>
2025-06-02 21:45:58 +0300

    Created 'A - Can I Square?' by A2SV Hub

---
+++ A2SV Contest #13 02-Jun-2025/A - Can I Square? 362901.py
@@ -0,0 +1,31 @@
+# Problem: A - Can I Square? - https://codeforces.com/gym/609279/problem/A
+
+import math
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    total = sum(a)
+
+    l, r = 0, total
+    is_sq = False
+
+    while l <= r:
+        mid = (l + r) // 2
+        square = mid * mid
+
+        if square == total:
+            is_sq = True
+            break
+        elif square < total:
+            l = mid + 1
+        else:
+            r = mid - 1
+
+    if is_sq:
+        print("YES")
+    else:
+        print("NO")
