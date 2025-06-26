f8319e8b3505069717379b7c459c2dba437d1f85
Bisrat Teshome <135213040+b-isry@users.noreply.github.com>
2025-06-26 16:57:29 +0300

    Created 'Expression Add Operators' by A2SV Hub

---
+++ 26-Jun-2025/Expression Add Operators 370721.py
@@ -0,0 +1,25 @@
+# Problem: Expression Add Operators - https://leetcode.com/problems/expression-add-operators/description/
+
+class Solution:
+    def addOperators(self, num: str, target: int):
+        answer = []
+        def back( position, expression):
+            #basecase
+            if len(num) == position:
+                if eval(expression) == target:
+                    answer.append(expression) 
+                return 
+
+            for i in range(position + 1, len(num) + 1):
+                temp = num[position: i]
+                if len(temp) > 1 and temp[0] == "0":
+                    continue
+                if position == 0:
+                    back( i,temp)
+                else:
+                    back( i,expression + "+" + temp) 
+                    back( i,expression + "*" + temp) 
+                    back( i,expression + "-" + temp) 
+            return 
+        back(0,"")
+        return answer
