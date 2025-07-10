e6426fbe45162024f9a5180930a696dbdb81ed85
Bisrat Teshome <135213040+b-isry@users.noreply.github.com>
2025-07-10 23:03:16 +0300

    Created 'Find Median from Data Stream' by A2SV Hub

---
+++ 10-Jul-2025/Find Median from Data Stream 375291.py
@@ -0,0 +1,29 @@
+# Problem: Find Median from Data Stream - https://leetcode.com/problems/find-median-from-data-stream/
+
+class MedianFinder:
+
+    def __init__(self):
+        self.low = []
+        self.high = []
+        
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.low, -num)
+        heapq.heappush(self.high, -self.low[0])
+        heapq.heappop(self.low)
+
+        if len(self.low) < len(self.high):
+            heapq.heappush(self.low, -self.high[0])
+            heapq.heappop(self.high)
+
+    def findMedian(self) -> float:
+        if len(self.low) > len(self.high):
+            return -self.low[0]
+        else:
+            return (self.high[0] - self.low[0]) / 2
+        
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
