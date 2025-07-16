2b464fa314a6a054d1629fd67d5792991ea6cc01
Bisrat Teshome <135213040+b-isry@users.noreply.github.com>
2025-07-16 23:18:37 +0300

    Created 'Jump Game' by A2SV Hub

---
+++ 16-Jul-2025/Jump Game 377135.py
@@ -0,0 +1,10 @@
+# Problem: Jump Game - https://leetcode.com/problems/jump-game/
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        farthest = 0
+        for i in range(len(nums)):
+            if i > farthest:
+                return False  
+            farthest = max(farthest, i + nums[i])
+        return True
