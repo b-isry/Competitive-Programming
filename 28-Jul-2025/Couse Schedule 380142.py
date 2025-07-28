7bc70da2d7cc01bf0147e4ce125d52e3bcfed452
Bisrat Teshome <135213040+b-isry@users.noreply.github.com>
2025-07-28 23:51:25 +0300

    Created 'Couse Schedule' by A2SV Hub

---
+++ 28-Jul-2025/Couse Schedule 380142.py
@@ -0,0 +1,23 @@
+# Problem: Couse Schedule - https://leetcode.com/problems/course-schedule/
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        indeg = [0] * numCourses
+        g = [[] for i in range(numCourses)]
+        res = []
+        
+        for u,v in prerequisites: 
+            g[u].append(v)
+            indeg[v] += 1
+        q = deque(i for i in range(numCourses) if indeg[i] == 0)
+
+        while q:
+            course = q.popleft()
+            res.append(course)
+            for nei in g[course]:
+                indeg[nei] -= 1
+                if indeg[nei] == 0:
+                    q.append(nei)
+        if len(res) != numCourses:
+            return False
+        return True
