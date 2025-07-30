bf6ac97e1dba00e81067240c3b20bfcf3337fbb9
Bisrat Teshome <135213040+b-isry@users.noreply.github.com>
2025-07-30 22:33:02 +0300

    Created 'Climbing Stairs' by A2SV Hub

---
+++ 30-Jul-2025/Climbing Stairs 380667.py
@@ -0,0 +1,14 @@
+# Problem: Climbing Stairs - https://leetcode.com/problems/climbing-stairs/
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        def dp(i):
+            if i < 0:
+                return 0
+            if i == 1 or i == 0:
+                return 1
+            if i not in memo:
+                memo[i] = dp(i-1) + dp(i-2)
+            return memo[i]
+        return dp(n)
