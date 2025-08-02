358b3a5c5c1c0fa430bda8b14efe3053f5b84d6c
Bisrat Teshome <135213040+b-isry@users.noreply.github.com>
2025-08-02 19:28:38 +0300

    Created 'Longest Increasing Subsequence' by A2SV Hub

---
+++ 02-Aug-2025/Longest Increasing Subsequence 381375.py
@@ -0,0 +1,10 @@
+# Problem: Longest Increasing Subsequence - https://leetcode.com/problems/longest-increasing-subsequence/
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        memo = [1]*len(nums)
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i]>nums[j]:
+                    memo[i]=max(memo[i], memo[j]+1)
+        return max(memo)
