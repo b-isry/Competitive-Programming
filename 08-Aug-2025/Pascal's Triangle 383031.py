7bef126af02a0e2686594344118ab314576fedfa
Bisrat Teshome <135213040+b-isry@users.noreply.github.com>
2025-08-08 22:55:28 +0300

    Created 'Pascal's Triangle' by A2SV Hub

---
+++ 08-Aug-2025/Pascal's Triangle 383031.py
@@ -0,0 +1,12 @@
+# Problem: Pascal's Triangle - https://leetcode.com/problems/pascals-triangle/description/
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        ans = [[1]]
+        for i in range(1,numRows):
+            row = [1]
+            for j in range(1,i):
+                row.append(ans[i-1][j-1] + ans[i-1][j])
+            row.append(1)
+            ans.append(row) 
+        return ans
